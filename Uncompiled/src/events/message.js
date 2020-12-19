eca55eb9f4bd4a50ae43496f5807f53e5cf4157b
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-12-19 03:02:49 +0000

    Update message.js

--- Uncompiled/src/events/message.js
+++ Uncompiled/src/events/message.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 
 const { spawn, exec } = require("child_process");
 module.exports = async (client, message) => {
-	if (!message.content.startsWith(settings.prefix)) return;
-	const args = message.content.slice(settings.prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-	try {
-		await client.commands.get(command).execute(client, message, args);
-	} catch (error) {
-		return;
-	}
+	//if (!message.content.startsWith(settings.prefix)) return;
+	//const args = message.content.slice(settings.prefix.length).trim().split(/ +/);
+	//const command = args.shift().toLowerCase();
+	//try {
+		//await client.commands.get(command).execute(client, message, args);
+	//} catch (error) {
+		//return;
+	//}
 }

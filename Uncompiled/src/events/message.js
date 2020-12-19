2f53debbf7e1ddf0e38d6baafc57bef1c80c593b
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-12-19 13:44:24 +0000

    Update message.js

--- Uncompiled/src/events/message.js
+++ Uncompiled/src/events/message.js
@@ -3,49 +3,13 @@ const {
 	prefix
 } = require('../../kosmos.json');
 const fs = require("fs");
-
-module.exports = async (message) => {
-	const client = message.client;
-	console.log('BRUH');
-	if (!message.content.startsWith(prefix) || message.author.id != message.client.id) return;
-
+module.exports = async (client, message) => {
+	if (!message.content.startsWith(prefix)) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	if (commandName === 'ping') message.channel.send('bruh');
-	const command = client.commands.get(commandName) ||
-		client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	if (command.guildOnly && message.channel.type === 'dm') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
+	const command = args.shift().toLowerCase();
+	try {
+		await client.commands.get(command).execute(client, message, args);
+	} catch (error) {
+		return;
 	}
 }

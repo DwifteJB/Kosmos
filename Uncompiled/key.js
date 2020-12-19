53caf606f64d69079d94b0562994011d5cc62c0d
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-12-19 03:02:14 +0000

    Update key.js

--- Uncompiled/key.js
+++ Uncompiled/key.js
@@ -104,7 +104,7 @@ const Login = async keyPrompt => {
   }
   if (client.token == undefined) return console.log("The token in kosmos.json was invalid and we couldn't connect to the discord api.");
   
-  let Terminal = "On";
+  let terminal = "On";
   console.log("Kòsmos Terminal, type help for commands!\n\nKòsmos created by DwifteJB and Thunder7Yoshi");
   while (terminal == "On") {
     try {

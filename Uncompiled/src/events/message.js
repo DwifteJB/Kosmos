7a7d3c7539a93e07761ca288a28a627d5d93a868
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-12-22 03:36:53 +0000

    Update message.js

--- Uncompiled/src/events/message.js
+++ Uncompiled/src/events/message.js
@@ -5,8 +5,10 @@ const {
 const fs = require("fs");
 module.exports = async (client, message) => {
 	if (!message.content.startsWith(prefix)) return;
+        if (!message.member.id == message.member.id) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
+        console.log(command);
 	try {
 		await client.commands.get(command).execute(client, message, args);
 	} catch (error) {

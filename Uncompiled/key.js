59785f56fa8f2d81ece498bebe9eb14d3dbcd52d
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-12-22 02:52:09 +0000

    Update key.js

--- Uncompiled/key.js
+++ Uncompiled/key.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
- const prompt = require('prompt-sync')({		 const prompt = require('prompt-sync');
+ const prompt = require('prompt-sync')({
    sigint: true		
  });
 

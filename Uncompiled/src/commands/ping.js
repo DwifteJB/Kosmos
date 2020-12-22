6ae0709ac12b5f7fcd899fd1a244cd68b2bde9c3
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-12-22 04:05:47 +0000

    Update ping.js

--- Uncompiled/src/commands/ping.js
+++ Uncompiled/src/commands/ping.js
@@ -3,7 +3,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	async execute(client, message, args) {
-		const msg = await message.channel.send('Ping?');
-		msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms.`)
+		message.channel.send('Ping?');
 	},
 };
